4d262e1a37d3746b5c0d173e2149bcc8ffd1767c
Dami K <dmk9104@gmail.com>
2024-09-08 23:29:59 +0900

    build config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
+    basePath: "/next-portfolio",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
