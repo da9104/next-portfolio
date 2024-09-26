3bff74d73044c96f4f92582d9be436019d526ca3
Dami K <dmk9104@gmail.com>
2024-09-27 07:21:40 +0900

    Adjusted modal popup link color

--- src/components/Header/Nav/Footer/index.jsx
+++ src/components/Header/Nav/Footer/index.jsx
@@ -5,7 +5,6 @@ export default function index() {
   return (
     <div className={styles.footer}>
         <a>Blog</a>
-        <a>Instagram</a>
         <a href={'https://www.behance.net/d9104'}>Behance</a>
         <a>LinkedIn</a>
     </div>
